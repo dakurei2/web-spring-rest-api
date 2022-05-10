24cccef00bf07823b89544381ee30ce779b4f9a9
dakurei2 <dakurei123@gmail.com>
2022-05-10 15:52:23 +0700

    Add files via upload

---
+++ src/main/java/tacos/data/OrderRepository.java
@@ -0,0 +1,9 @@
+package tacos.data;
+
+import org.springframework.data.repository.CrudRepository;
+
+import tacos.model.Order;
+
+public interface OrderRepository extends CrudRepository<Order, String> {
+
+}
